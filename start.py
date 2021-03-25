57535eb4d4b14e5eb62ee799562bb21b7fcb7800
Walt yao <walt.yao@gmail.com>
2021-03-25 16:29:29 +0800

    added form

--- start.py
+++ start.py
@@ -1,10 +1,28 @@
 from flask import Flask           # import flask
+from wtforms import Form, FloatField, validators, SubmitField
 app = Flask(__name__)             # create an app instance
 
 
-@app.route("/<param>")                   # at the end point /
-def letsgo(param):                      # call method hello
-    return "Hello " + param         # which returns "hello world"
+class MoneyForm(Form):
+    amount = FloatField('Username', [validators.Length(min=4, max=25)])
+    amount_minus = SubmitField()
+    amount_add = SubmitField()
+
+# @app.route("/<param>")                   # at the end point /
+# def lets_go(param):                      # call method hello
+#     return "Hello " + param         # which returns "hello world"
+
+
+@app.route("/")                   # at the end point /
+def begin(request):                      # call method hello
+    form = MoneyForm(request.POST)
+    # if request.method == 'POST' and form.validate():
+    #     user = User()
+    #     user.username = form.username.data
+    #     user.email = form.email.data
+    #     user.save()
+    #     redirect('register')
+    # return render_response('register.html', form=form)
 
 
 # if __name__ == "__main__":        # on running python app.py
