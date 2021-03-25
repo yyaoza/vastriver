dfaba66d9ce0cdb4dc573d0129ef19c1ff337dc0
Walt Yao <waltyao@Walts-MacBook-Pro-2.local>
2021-03-25 14:53:24 +0800

    initial true commit

---
+++ start.py
@@ -0,0 +1,11 @@
+from flask import Flask           # import flask
+app = Flask(__name__)             # create an app instance
+
+
+@app.route("/<param>")                   # at the end point /
+def hello(param):                      # call method hello
+    return "Hello " + param         # which returns "hello world"
+
+
+if __name__ == "__main__":        # on running python app.py
+    app.run(debug=True)                     # run the flask app
