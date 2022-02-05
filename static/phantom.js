cf71dec8967bacf7c5373fbb3bf9005ec0c45e73
Walt <walt.yao@gmail.com>
2022-02-05 21:30:51 +0800

    phantom wallet connection and balance retrieval

--- static/phantom.js
+++ static/phantom.js
@@ -1,25 +1,52 @@
-document.getElementById("login").addEventListener("click", myFunction);
-function myFunction() {
+document.getElementById("login").addEventListener("click", login);
+document.getElementById("disconnect").addEventListener("click", disconnect);
 
-    async function fetchHtmlAsText(url) {
-        return await (await fetch(url)).text();
-    }
+function disconnect() {
+    window.solana.disconnect();
+    document.getElementById("login").innerHTML = '<i class="bi bi-wallet2"></i> Connect';
+    document.getElementById("disconnect").style.display = "none";
+};
+
+function login() {
 
     const getProvider = async () => {
       if ("solana" in window) {
         await window.solana.connect(); // opens wallet to connect to
         const provider = window.solana;
         if (provider.isPhantom) {
-          console.log("Is Phantom installed?  ", provider.isPhantom);
-          publicKey_string = provider.publicKey.toString()
-          publicKey_short = publicKey_string.substring(0, 3) + "..." + publicKey_string.slice(publicKey_string.length - 3)
-          document.getElementById("login").innerHTML = await fetchHtmlAsText("static/top_bar.html") + publicKey_short + "</button>";
+            console.log("Is Phantom installed?  ", provider.isPhantom);
+            publicKey_string = provider.publicKey.toString()
+            publicKey_short = publicKey_string.substring(0, 3) + "..." + publicKey_string.slice(publicKey_string.length - 3)
+            document.getElementById("login").innerHTML = '<i class="bi bi-wallet2"> ' + publicKey_short + "  </i></button>";
+            document.getElementById("disconnect").style.display = "block";
+
+            console.log("Wallet Connected: " + provider.isConnected);
+            if (phantom.isConnected !== false) {
+
+                const wallet = new solanaWeb3.PublicKey(provider.publicKey.toString());
+
+                // connect to the cluster
+                console.log("Connecting Cluster");
+                var connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl('devnet'), 'confirmed');
+
+                // remember, do not use base 58 encoded key with getBalance();
+                console.log("Getting Balance: " + publicKey_string);
+                // After Connecting
+//                connection.getBalance(wallet).then(function(value) { console.log(value); });
+
+                const balance = await connection.getBalance(provider.publicKey)
+                console.log(balance);
+
+            }
+
           return provider;
         }
+        window.open("https://phantom.app/", "_blank");
       } else {
-        document.write('Install https://www.phantom.app/');
+        window.open("https://phantom.app/", "_blank");
       }
     };
+
 //    window.onload = () => {
       getProvider().then(provider => {
         console.log('key', provider.publicKey.toString())
@@ -27,5 +54,12 @@ function myFunction() {
       .catch(function(error){
         console.log(error)
       });
+
+//      phantom_balance().then(balance => {
+//        console.log('key', balance)
+//      })
+//      .catch(function(error){
+//        console.log(error)
+//      });
 //    }
 };
